f32e5e71093488709df70bc76594345a7d3298df
Pichaaun Popukdee <pichaaun.p@ku.th>
2018-03-16 21:08:35 +0700

    Update Controller.java

--- src/Controller.java
+++ src/Controller.java
@@ -5,6 +5,12 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 import java.util.Random;
+/**
+*
+* Controller for consolefx FXML contain with handleguessing , invalid and exit.
+* @author Pichaaun Popukdee
+*
+*/
 
 
 public class Controller extends NumberGame {
@@ -21,6 +27,7 @@ public class Controller extends NumberGame {
     @FXML
     Button reset;
 
+    /**Set the secret number**/
     public void setSecret(int upperBound) {
         this.secret = ran.nextInt(upperBound) + 0;
     }
@@ -35,7 +42,12 @@ public class Controller extends NumberGame {
         this.guessing = counter;
     }
 
+    /**
+    * Event that will show on the consolefx most has for check the input number with secret number.
+    *
+    */
     public boolean handleGuess(ActionEvent event) {
+        try{
         guessing.add(1);
         String text = textField.getText().trim();
         if (textField==null) invalid(textField);
@@ -50,24 +62,30 @@ public class Controller extends NumberGame {
         } else if (number > secret) {
             Hint.setText("It's too large!");
 
-            return false;
+            return false;}
+            catch(Exception e){
+            invalid(textField)
+            }
         }
         return false;
     }
 
 
     @FXML
+    /**If there is wrong in put or nothing on textField the border will be red.*/
     public void invalid (TextField textField) {
         textField.setStyle(" -fx-border-color: darkred");
     }
 
+    /**Reset all game*/
     public void reset() {
         guessing.setCount(0);
         textField.clear();
         Hint.clear();
         secret = ran.nextInt(999+0);
     }
-
+    
+    /**Close the game*/
     public void exit () {
         System.exit(2);
     }
